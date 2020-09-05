4a3f6378af115023fd8848c466f4dfe9cb48c13a
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2020-09-05 19:14:33 +0800

    Seem like workable. No crash :-)

---
+++ TabDemo/TabDemo/tab/TabCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TabCollectionViewCell.swift
+//  TabDemo
+//
+//  Created by guest2 on 05/09/2020.
+//  Copyright © 2020 yocto. All rights reserved.
+//
+
+import UIKit
+
+class TabCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

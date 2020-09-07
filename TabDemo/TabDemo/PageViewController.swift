c13b0920e0306b479a2e6bc0db81e74823fc209c
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2020-09-07 13:19:03 +0800

    Added PageViewController.

---
+++ TabDemo/TabDemo/PageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PageViewController.swift
+//  TabDemo
+//
+//  Created by guest2 on 07/09/2020.
+//  Copyright © 2020 yocto. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

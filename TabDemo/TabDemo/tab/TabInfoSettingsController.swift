bb0b6fe5e323e5deabfad0adbf09579f0871dd1a
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2020-09-08 12:42:34 +0800

    Support TabInfoSettingsController

---
+++ TabDemo/TabDemo/tab/TabInfoSettingsController.swift
@@ -0,0 +1,30 @@
+//
+//  TabInfoSettingsController.swift
+//  TabDemo
+//
+//  Created by guest2 on 08/09/2020.
+//  Copyright © 2020 yocto. All rights reserved.
+//
+
+import UIKit
+
+class TabInfoSettingsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

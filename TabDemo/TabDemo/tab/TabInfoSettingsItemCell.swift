2e3335fe6e5ab181f3a4758ce763d885cb7b2731
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2020-09-10 18:52:37 +0800

    WIP

---
+++ TabDemo/TabDemo/tab/TabInfoSettingsItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  TabInfoSettingsItemCell.swift
+//  TabDemo
+//
+//  Created by guest2 on 10/09/2020.
+//  Copyright © 2020 yocto. All rights reserved.
+//
+
+import UIKit
+
+class TabInfoSettingsItemCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

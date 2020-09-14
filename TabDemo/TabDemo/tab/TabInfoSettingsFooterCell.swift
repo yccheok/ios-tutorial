188470d781ef9355742a5538bfce1f335608ad29
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2020-09-14 10:32:16 +0800

    Support footer.

---
+++ TabDemo/TabDemo/tab/TabInfoSettingsFooterCell.swift
@@ -0,0 +1,18 @@
+//
+//  TabInfoSettingsFooterCell.swift
+//  TabDemo
+//
+//  Created by guest2 on 14/09/2020.
+//  Copyright © 2020 yocto. All rights reserved.
+//
+
+import UIKit
+
+class TabInfoSettingsFooterCell: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}

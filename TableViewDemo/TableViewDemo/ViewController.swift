d0748eb805a19da56f8c737c4a0c1cac372c1adc
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2020-09-16 23:59:15 +0800

    First try in TableView.

---
+++ TableViewDemo/TableViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewDemo
+//
+//  Created by guest2 on 16/09/2020.
+//  Copyright © 2020 yocto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

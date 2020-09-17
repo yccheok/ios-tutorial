040fff1b3fe7406daf5987c6bfbd9d0b28b195b2
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2020-09-17 11:49:23 +0800

    Embed with NavigationController

--- TableViewDemo/TableViewDemo/ViewController.swift
+++ TableViewDemo/TableViewDemo/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var movies = ["The Lion King", "The Incredibles", "Guardians of the Galaxy", "The Flash", "The Avengers", "The Dark Knight", "The Walking Dead", "Insidious", "Conguring"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
+}
 
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = movies[indexPath.row]
+        
+        return cell
+    }
 }
 

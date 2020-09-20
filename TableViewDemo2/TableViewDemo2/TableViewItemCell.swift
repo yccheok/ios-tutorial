61dbfb2ef1529ddb48f38bb63895c8e3b5b25f6d
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2020-09-20 17:29:30 +0800

    WIP

---
+++ TableViewDemo2/TableViewDemo2/TableViewItemCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewItemCell.swift
+//  TableViewDemo2
+//
+//  Created by guest2 on 20/09/2020.
+//
+
+import UIKit
+
+class TableViewItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

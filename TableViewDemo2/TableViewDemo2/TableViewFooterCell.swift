46e7f0dfcbc71feeca7c8fb9af0b59235f5b1770
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2020-09-20 17:29:46 +0800

    ...

--- TableViewDemo2/TableViewDemo2/TableViewFooterCell.swift
+++ TableViewDemo2/TableViewDemo2/TableViewFooterCell.swift
@@ -7,17 +7,11 @@
 
 import UIKit
 
-class TableViewFooterCell: UITableViewCell {
+class TableViewFooterCell: UITableViewHeaderFooterView {
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
 }

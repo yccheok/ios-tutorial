e342a55ddfaf6c3b5c114151aad016ea70c28c14
yccheok <yancheng.cheok@gmail.com>
2021-01-04 12:48:00 +0800

    WIP

---
+++ experiment-subview/experiment-subview/CustomView2.swift
@@ -0,0 +1,20 @@
+//
+//  CustomView2.swift
+//  experiment-subview
+//
+//  Created by Cheok Yan Cheng on 04/01/2021.
+//
+
+import UIKit
+
+class CustomView2: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ experiment-subview/experiment-subview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  experiment-subview
+//
+//  Created by Cheok Yan Cheng on 04/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

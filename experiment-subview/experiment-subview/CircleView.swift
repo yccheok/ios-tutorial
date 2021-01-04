ecc325ac11e20d785fe4eb4d5c34c12f5709a618
yccheok <yancheng.cheok@gmail.com>
2021-01-04 17:46:39 +0800

    Introduce circle view.

---
+++ experiment-subview/experiment-subview/CircleView.swift
@@ -0,0 +1,20 @@
+//
+//  CircleView.swift
+//  experiment-subview
+//
+//  Created by Cheok Yan Cheng on 04/01/2021.
+//
+
+import UIKit
+
+class CircleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

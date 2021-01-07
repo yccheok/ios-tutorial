951e74d41e69713160440fe61ddf7d284c80d13d
yccheok <yancheng.cheok@gmail.com>
2021-01-07 22:35:31 +0800

    WIP

---
+++ experiment-subview/experiment-subview/CustomColorView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomColorView.swift
+//  experiment-subview
+//
+//  Created by Cheok Yan Cheng on 06/01/2021.
+//
+
+import UIKit
+
+class CustomColorView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ experiment-subview/experiment-subview/PresetColorView.swift
@@ -0,0 +1,20 @@
+//
+//  PresetColorView.swift
+//  experiment-subview
+//
+//  Created by Cheok Yan Cheng on 06/01/2021.
+//
+
+import UIKit
+
+class PresetColorView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
